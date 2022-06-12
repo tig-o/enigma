87c51a37c27f1f92f3a46df8677666fcd34a3c59
Thiago Silveira <thiago1992@yahoo.com>
2022-06-11 21:52:00 -0600

    Test: Enigma can encrypt a message with a key and date

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Enigma do
     enigma = Enigma.new
     expect(enigma).to be_a(Enigma)
   end
+
+  it 'can encrypt a message with a key and date' do
+    enigma = Enigma.new
+
+    expected =
+    {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    
+    expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+  end
 end
