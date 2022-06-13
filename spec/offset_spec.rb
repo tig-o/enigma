98a44ae815eed190727790b87b9972844deb520c
Thiago Silveira <thiago1992@yahoo.com>
2022-06-13 11:12:58 -0600

    Test: Offset can return a manually entered DDMMYY date

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Offset do
     offset = Offset.new
     expect(offset).to be_a(Offset)
   end
+
+  it 'can return a manually entered DDMMYY date' do
+    offset = Offset.new
+    offset.manual_date = "032592"
+    expect(offset.manual_date).to eq("032592")
+  end
 end
