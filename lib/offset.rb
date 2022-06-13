b72dccfd3af24c441d7f79460dc8e545f0ba8aa3
Thiago Silveira <thiago1992@yahoo.com>
2022-06-13 14:07:55 -0600

    Feat: Pass test offset assigns A B C D offsets

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,16 +1,26 @@
 class Offset
   attr_accessor :manual_date,
                 :todays_date,
-                :calculated_offset
+                :calculated_offset,
+                :offset_array
 
   def initialize
     @manual_date = String.new
     @todays_date = Date.today.strftime('%m%d%y')
     @current_date = String.new
     @calculated_offset = Array.new
+    @offset_array = Array.new
   end
 
   def calculate_offset(date)
     @calculated_offset = (date.to_i * date.to_i).to_s[6..-1].chars
+  end
+
+  def assign_offsets
+    a_offset = @calculated_offset[0]
+    b_offset = @calculated_offset[1]
+    c_offset = @calculated_offset[2]
+    d_offset = @calculated_offset[3]
+    @offset_array = [a_offset, b_offset, c_offset, d_offset]
   end
 end
