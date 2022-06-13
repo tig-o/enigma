cc735ed0b4be9f3e0e0b2fa661ca05f01cb391ce
Thiago Silveira <thiago1992@yahoo.com>
2022-06-13 13:48:18 -0600

    Feat: Pass test offset calculates starting offset

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,13 +1,16 @@
 class Offset
   attr_accessor :manual_date,
-                :todays_date
+                :todays_date,
+                :calculated_offset
 
   def initialize
     @manual_date = String.new
-    @todays_date = Date.today.to_s
+    @todays_date = Date.today.strftime('%m%d%y')
+    @current_date = String.new
+    @calculated_offset = Array.new
   end
 
-  def format_date
-    @todays_date = Date.today.strftime('%m%d%y')
+  def calculate_offset(date)
+    @calculated_offset = (date.to_i * date.to_i).to_s[6..-1].chars
   end
 end
