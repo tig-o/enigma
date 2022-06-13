61ad049104080a1586f6d0f7b6d36e777bd5227f
Thiago Silveira <thiago1992@yahoo.com>
2022-06-13 00:13:59 -0600

    Test: Key can return a manually entered key

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Key do
     key = Key.new
     expect(key).to be_a(Key)
   end
+
+  it 'can return a manually entered key' do
+    key = Key.new
+    key.manual_key = "02513"
+    expect(key.manual_key).to eq("02513")
+  end
+
 end
