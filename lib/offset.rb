06a3bfb836c72b3e10cbd9d886835ce9277a3ee6
Thiago Silveira <thiago1992@yahoo.com>
2022-06-13 11:47:28 -0600

    Feat: Pass test offset returns todays date

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,6 +1,9 @@
 class Offset
-  attr_accessor :manual_date
+  attr_accessor :manual_date,
+                :todays_date
+
   def initialize
     @manual_date = String.new
+    @todays_date = Date.today.to_s
   end
 end
