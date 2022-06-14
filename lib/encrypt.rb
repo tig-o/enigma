57a6455296046f175bda170eef27957f4a1ea741
Thiago Silveira <thiago1992@yahoo.com>
2022-06-14 13:16:58 -0600

    Feat: Add encrypt runner cli output message

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -6,11 +6,13 @@ enigma = Enigma.new
 message_text = ARGV[0]
 encrypted_text = ARGV[1]
 
+
 message = File.new(message_text).read
 
 File.open(encrypted_text, "w") do |text_file|
   enigma.encrypt(message) # reads what is inside message text and passes through to enigma.rb
   text_file.puts enigma.encrypted_hash[:encryption] # writes encrypted message to message text file
+  puts "Created #{encrypted_text} with the key #{enigma.encrypted_hash[:key]} and date #{enigma.encrypted_hash[:date]}."
 end
 # $ ruby ./lib/encrypt.rb message.txt encrypted.txt
 # Created 'encrypted.txt' with the key 82648 and date 240818
